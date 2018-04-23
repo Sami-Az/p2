a310f531dd4dee9ce8a651d9c0e8036c06b458ce
Sami Abderrezak <sami.abderre@gmail.com>
2018-04-23 21:24:59 +0200

    home links function now

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,26 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+// Log-in 
+router.get ('/login', (req, res, next) => {
+  res.render('auth/login-page');
+});
+
+
+// Sign-up
+router.get ('/signup', (req, res, next) => {
+  res.render('auth/signup-page');
+});
+
+// About us
+router.get ('/about', (req, res, next) => {
+  res.render('about-page');
+});
+
+// Contact us
+router.get ('/contact', (req, res, next) => {
+  res.render('contact-page');
+});
+
+
 module.exports = router;
